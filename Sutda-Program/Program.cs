bbcd1b1fdf1453ab7b580134d1f87a371a1f2cae
kmdoyeon <127840494+jssjssjss@users.noreply.github.com>
2023-04-24 19:50:46 +0900

    Sutda-Program
    Dictionary<string, int> jokbo = new Dictionary<string, int>()
    {
    
    }

---
+++ Sutda-Program/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+                //1    2   3    4    5    6    7    8    9   10
+string[] deck = {"a", "b","c", "d", "e", "f", "g", "h", "i","j",
+                 "A", "B","C", "D", "E", "F", "G", "H", "I", "J"};
+
+
+Dictionary<string, int> jokbo = new Dictionary<string, int>()
+{
+    //dictionary에 10땡 까지 추가하기
+    { "aA" , 200 },{ "bB" , 220 },{ "cC" , 240 },{ "dD" , 260 },{ "eE" , 280 },{ "fF" , 300 },{ "gG" , 320 },{ "hH" , 340 },{ "iI" , 360 },{ "jJ" , 380 },
+
+
+};
+
+
+List<string> computer1 = new List<string>();
+List<string> player0 = new List<string>();
+
+
+player0.Add(deck[0]);
+player0.Add(deck[1]);
+
+computer1.Add(deck[0]);
+computer1.Add(deck[1]);
+
+
+
+string player0_value=player0[0]+player0[1];
+Console.WriteLine(player0_value);
+
+Console.WriteLine(jokbo[player0_value]);
+
---
+++ Test/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+String[] dec = {"1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a",
+                "1b", "2b", "3b", "4b", "5b", "6b", "7b", "8b", "9b", "10b" };
+
+
+List<string> mycard = new List<string>();
+List<int> mycard_onlynum = new List<int>();
+List<string> mycard_onlyshape = new List<string>();
+List<int> randnum_list = new List<int>();
+
+Random random = new Random();
+
+//플레이어  4명
+//2장씩 겹치지 않고 분배
+
+for (int i = 0; i < 2; i++)
+{
+
+    int a = random.Next(0, 20);
+
+    if (!randnum_list.Contains(a))
+    {
+        randnum_list.Add(a);
+    }
+
+
+    Console.WriteLine(randnum_list);
+
+
+
+}
+
+
+for (int i = 0; i < mycard.Count; i++)
+{
+    mycard_onlynum.Add(Convert.ToInt32(mycard[i].Substring(0, 1)));
+    mycard_onlyshape.Add(mycard[i].Substring(1, 1));
+
+}
+
+
+mycard_onlynum.Sort();
+mycard_onlyshape.Sort();
+
+
+for (int i = 0; i < mycard.Count; i++)
+{
+    Console.WriteLine(mycard_onlynum + "" + mycard_onlyshape);
+
+}
+
+
+
+
+
+
+//38광땡
+
+//13광떙 18광떙
+
+//94 재경기
+
+//1~10떙
+
+//
