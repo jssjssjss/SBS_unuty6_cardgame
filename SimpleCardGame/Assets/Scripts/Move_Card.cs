7d891425e49c0ec45d89cdbba927e69780301900
kmdoyeon <127840494+jssjssjss@users.noreply.github.com>
2023-04-26 19:50:34 +0900

    0426
    -Vector3
    -my_rigid.MovePosition()
    -transfor.position = Vector3.MoveTowards

--- SimpleCardGame/Assets/Scripts/Move_Card.cs
+++ SimpleCardGame/Assets/Scripts/Move_Card.cs
@@ -6,8 +6,13 @@ public class Move_Card : MonoBehaviour
 {
     Rigidbody2D my_rigid;
     //백터 위치를 받는것 = vector2
-    Vector2 my_position;
-    Vector2 goal_position;
+    Vector3 my_position;
+
+    [SerializeField]
+    Vector3 goal_position;
+    Vector3 Next_position;
+
+    
 
 
 [SerializeField] 
@@ -15,22 +20,45 @@ public class Move_Card : MonoBehaviour
 
     void Start()
     {
+
         my_rigid=GetComponent<Rigidbody2D>();
         my_position=transform.position;
 
         goal_position=goal.transform.position;
+        //c-a
+        Next_position = my_position + (goal_position - my_position);
 
-        
+        ///                       내위치          
+      // transform.position=transform.position+Next_position;
+
+
+       
+       // transform.position=Next_position;
+
+
 
         Debug.Log("my_position :" + my_position);
         Debug.Log("goal_position :" + goal_position);
+        Debug.Log("Next_position :" + Next_position);
+        Debug.Log("Next_position :" + Next_position.normalized);
         
     }
 
 
     // Update is called once per frame
-    void Update()
+    void Update() // 한 프레임마다 호출
     {
-        
+
+
+        Next_position = goal_position - transform.position;
+
+        // transform.position = transform.position+ Next_position * Time.deltaTime;
+
+        //my_rigid.MovePosition(transform.position + Next_position.normalized *5* Time.deltaTime);
+
+
+        //                           정확하게도          시작            도착             시간/속도
+        transform.position = Vector3.MoveTowards(transform.position, goal_position, 10 * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, Next_position, 10 * Time.deltaTime);
     }
 }
